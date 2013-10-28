e4652f8c43acc973e5c787c028e35db57390de31
Todd Persen <todd.persen@gmail.com>
2013-10-28 13:33:33 -0400

    First commit. Rails 4.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Play::Application.config.secret_key_base = 'c700f6a78d5fab3a4a604b426cf4a2db96ad1e7e1ed66bdc5605cdb1aa516a44bc496e4533c6082069491fd78dbdeb7bef979949201e4aa2dd95c4b7785014c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Play::Application.config.session_store :cookie_store, key: '_Play_session'
