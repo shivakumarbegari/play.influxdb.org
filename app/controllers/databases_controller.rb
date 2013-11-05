e57194e8c3f1d74b3f10ac94b26cfa6573a3bcd8
Todd Persen <todd.persen@gmail.com>
2013-11-05 16:39:20 -0500

    Change root password. And hide it, since nobody can be trusted, apparently.

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -7,7 +7,7 @@ class DatabasesController < ApplicationController
     password = params[:database][:password]
     database = params[:database][:name]
 
-    @influxdb = InfluxDB::Client.new("sandbox.influxdb.org", 9061, "root", "root", nil)
+    @influxdb = InfluxDB::Client.new("sandbox.influxdb.org", 9061, "root", INFLUXDB_ROOT_PASSWORD, nil)
     @influxdb.create_database_user(database, username, password)
 
     redirect_to "http://sandbox.influxdb.org:9062/#/?username=#{username}&password=#{password}&database=#{database}"
