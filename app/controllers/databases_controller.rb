967f8b5961853d4fe31efe2ebd9233fdd2788ec4
Todd Persen <todd.persen@gmail.com>
2013-11-06 10:28:12 -0500

    Don't send the password in the querystring.

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -10,6 +10,6 @@ class DatabasesController < ApplicationController
     @influxdb = InfluxDB::Client.new("sandbox.influxdb.org", 9061, "root", ENV["INFLUXDB_ROOT_PASSWORD"], nil)
     @influxdb.create_database_user(database, username, password)
 
-    redirect_to "http://sandbox.influxdb.org:9062/#/?username=#{username}&password=#{password}&database=#{database}"
+    redirect_to "http://sandbox.influxdb.org:9062/#/?username=#{username}&database=#{database}"
   end
 end
