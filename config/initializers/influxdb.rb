911023ba3b6fd85e582d7de3aa637d8c358c7199
Todd Persen <todd.persen@gmail.com>
2014-02-11 16:46:05 -0500

    Update initializer.

--- config/initializers/influxdb.rb
+++ config/initializers/influxdb.rb
@@ -3,8 +3,8 @@ INFLUXDB_ADMIN_PASS = "root"
 
 InfluxDB::Rails.configure do |config|
   config.influxdb_database = "influxdb-rails"
-  config.influxdb_username = "influxdb"
-  config.influxdb_password = "3rrpl4n3!"
+  config.influxdb_username = "root"
+  config.influxdb_password = ENV["INFLUXDB_ROOT_PASSWORD"]
   config.influxdb_host = "sandbox.influxdb.org"
   config.influxdb_port = "9061"
 end
